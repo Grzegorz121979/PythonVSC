97af76a0a97fbffe56cb5950b6c388efcb820c28
Grzegorzp1979 <pociechagrzegorz@gmail.com>
2022-12-27 18:57:06 +0100

    commit three

--- simple.py
+++ simple.py
@@ -1,11 +1,22 @@
 import random
 import string
 
-chars = string.ascii_letters + string.punctuation + string.digits
-chars = list(chars)
-key = chars.copy()
+plain_text = input("Enter a message: ")
 
-random.shuffle(key)
+def cod_string(plain_text):
+    chars = " " + string.ascii_letters + string.punctuation + string.digits
+    chars = list(chars)
+    key = chars.copy()
 
-print(chars)
-print(key)
+    random.shuffle(key)
+    cipher_text = ""
+
+    for letter in plain_text:
+        index = chars.index(letter)
+        cipher_text += key[index]
+    
+    return cipher_text
+
+print(plain_text)
+coding_string = cod_string(plain_text)
+print(coding_string)
