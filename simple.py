9e133a6643b15371b3a1a1e97f89aad982ae1ea9
Grzegorzp1979 <pociechagrzegorz@gmail.com>
2022-12-27 20:29:54 +0100

    decoding function

--- simple.py
+++ simple.py
@@ -4,25 +4,41 @@ exercise coding string
 import random
 import string
 
+chars = " " + string.ascii_letters + string.punctuation + string.digits
+chars = list(chars)
+key = chars.copy()
+random.shuffle(key)
+
 plain_text = input("Enter a message: ")
 
 
-def cod_string(text):
+def cod_string(text, ch, k):
     '''
     function to coding string
     '''
-    chars = " " + string.ascii_letters + string.punctuation + string.digits
-    chars = list(chars)
-    key = chars.copy()
-
-    random.shuffle(key)
     cipher_text = ""
 
     for letter in text:
-        index = chars.index(letter)
-        cipher_text += key[index]
+        index = ch.index(letter)
+        cipher_text += k[index]
     
     return cipher_text
 
-print(f"The original text is: {plain_text}")
-print(f"The coded text is: {cod_string(plain_text)}")
+print(f"The coded text is: {cod_string(plain_text, chars, key)}")
+
+cipher_text = input("Enter a message: ")
+
+
+def decod_string(text, ch, k):
+    '''
+    function to decoding string
+    '''
+    plain_text = ""
+
+    for letter in cipher_text:
+        index = k.index(letter)
+        plain_text += ch[index]
+    
+    return plain_text
+
+print(f"The decoded text is: {decod_string(cipher_text, chars, key)}")
